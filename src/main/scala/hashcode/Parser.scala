c5268fea5755708aeea157c31fe72ee9750f13c7
Thibaut Robert <thibaut.robert@worldline.com>
2015-12-18 22:22:44 +0100

    parsing

--- src/main/scala/hashcode/Parser.scala
+++ src/main/scala/hashcode/Parser.scala
@@ -2,15 +2,27 @@ package hashcode
 
 object Parser {
   def read(): Problem = {
-    val input = io.Source.fromFile("input/data.in").getLines.toList
-    val first = input.head
-    val Array(nbRows, nbSlots, nbUnavailable, nbPools, nbServers) = first.split(" ").map(_.toInt)
+    val input = io.Source.fromFile("input/data.in").getLines
 
-    val servers = for {
-      (line, i) <- input.tail.drop(nbUnavailable).take(nbServers).zipWithIndex
-      Array(s, c) = line.split(" ")
-    } yield Server(s.toInt, c.toInt, i)
+    val Array(nbRows, nbCols, nbHeights) = input.next().split(" ").map(_.toInt)
+    val Array(nbCells, radius, nbBallons, nbTurns) = input.next().split(" ").map(_.toInt)
+    val Array(startRow,startCol) = input.next().split(" ").map(_.toInt)
 
-    Problem(servers)
+    val targetCells = List.fill(nbCells) { input.next().split(" ").map(_.toInt) }
+    val sections = List.fill(nbHeights) { List.fill(nbRows) {
+      val winds = input.next().split(" ").map(_.toInt).grouped(2)
+      winds.map({ case Array(row,col) => Vector(row,col) }).toList
+    } }
+
+    Problem(nbRows, nbCols, nbHeights, nbCells, radius, nbBallons, nbTurns, Point(startRow, startCol,0),
+      targetCells.map { case Array(x,y) => Point(x,y,0) },
+      sections.zipWithIndex.map { case (list, height) => list.zipWithIndex.map {
+        case (line, row) => line.zipWithIndex.map {
+          case (vector, col) => Point(row, col, height + 1) -> vector
+        }
+      }
+      }.flatten.flatten.toMap
+    )
+
   }
 }
--- src/main/scala/hashcode/Problem.scala
+++ src/main/scala/hashcode/Problem.scala
@@ -4,5 +4,15 @@ case class Server(size: Int, capacity: Int, id: Int) {
   def ratio: Float = capacity / size.toFloat
 }
 
-case class Problem(servers: List[Server])
+case class Point(row:Int, col:Int, height: Int) {
+
+}
+
+case class Vector(dr:Int, dc: Int)
+
+case class Problem(nbRows:Int, nbCols: Int, nbHeights:Int,
+                   nbCells:Int, radius:Int, nbBallons:Int, nbTurns:Int,
+                   startPoint: Point, targetCell: List[Point], winds: Map[Point, Vector]) {
+
+}
 
