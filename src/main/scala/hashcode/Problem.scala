6e45a58a444f2c33ebdba0db5b4403d94ec0bb45
Thibaut Robert <thibaut.robert@worldline.com>
2015-12-18 23:36:05 +0100

    validate position

--- src/main/scala/hashcode/Problem.scala
+++ src/main/scala/hashcode/Problem.scala
@@ -1,8 +1,16 @@
 package hashcode
 
 case class Point(row:Int, col:Int, height: Int) {
-  def addHeight(h: Int) = this.copy(height = height+h)   // TODO OVERFLOW !
-  def addVector(v:Vector) = this.copy(row= row + v.dr, col = (col + v.dc) % 300)
+  import Main.problem._
+  require(col< nbCols)
+  require(col>=0)
+  require(row< nbRows)
+  require(row>=0)
+  require(height< nbHeights)
+  require(height>=0)
+
+  def addHeight(h: Int) = this.copy(height = height+h)
+  def addVector(v:Vector) = this.copy(row= row + v.dr, col = (col + v.dc) % nbCols)
 }
 
 case class Vector(dr:Int, dc: Int)
