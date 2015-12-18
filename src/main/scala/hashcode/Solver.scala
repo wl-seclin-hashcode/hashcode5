0e8d76d39b0fb5830d3670a13aa77f48025d786a
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-12-18 23:00:35 +0100

    naive impl

--- src/main/scala/hashcode/Solver.scala
+++ src/main/scala/hashcode/Solver.scala
@@ -1,6 +1,15 @@
 package hashcode
 
 object Solver {
-  def solve(problem: Problem): Solution =
-    Solution(List(Command(1,1,1)))
+  def solve(problem: Problem): Solution = {
+    import problem._
+    val commands = for {
+      round <- 0 until nbTurns
+      balloon <- 0 until nbBallons
+    } yield {
+      val move = if (round % 8 == 0 && balloon == round / 8) 1 else 0
+      Command(balloon, move, round)
+    }
+    Solution(commands.toList)
+  }
 }
