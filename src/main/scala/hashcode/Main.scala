d0e25020322522cacc40260d7258056ee6538b8a
A518291 <A518291@EFR00946.fr01.awl.atosorigin.net>
2016-01-05 13:03:26 +0100

    visualize the wind map

--- src/main/scala/hashcode/Main.scala
+++ src/main/scala/hashcode/Main.scala
@@ -3,10 +3,12 @@ package hashcode
 import java.awt.{Dimension, Graphics}
 
 import scala.util.{Failure, Success}
+import scala.util.Random
 
 object Main extends App {
   val problem = Parser.read()
-  //showCalmSpots()
+  showWindMap()
+  //  showCalmSpots()
   val solution = Solver.solve(problem)
   val steps = Validator.states(solution, problem).map(_.ballons.toVector)
   Visualizer(paint, steps, problem, problem.nbTurns)
@@ -35,6 +37,28 @@ object Main extends App {
 
   }
 
+  def showWindMap() {
+    val windByHeight = problem.winds.groupBy { case (p, wv) => p.height }
+
+    def paintWind(g: Graphics, d: Dimension, p: Problem, step: Int): Unit = {
+      drawTargetCells(g, d, p)
+
+      for {
+        (point, wv) <- Random.shuffle(windByHeight(step).toList).take(100000)
+        (x, y) = coords(d, p, point.col, point.row)
+        point2 = point.addVector(wv)
+        (x2, y2) = coords(d, p, point2.col, point2.row)
+        if x2 - x > 0 && y2 - y > 0 && x2 - x < 30 && y2 - y < 100
+      } {
+        g.drawLine(x, y, x2, y2)
+        g.drawOval(x2, y2, 2, 2)
+      }
+    }
+
+    Visualizer(paintWind, 1 to 8, problem, problem.nbTurns)
+
+  }
+
   def paint(g: Graphics, d: Dimension, p: Problem, step: Vector[(Int, Point)]): Unit = {
     drawTargetCells(g, d, p)
 
@@ -54,6 +78,6 @@ object Main extends App {
     for {
       Point(row, col, _) <- p.targetCells
       (x, y) = coords(d, p, col, row)
-    } g.drawString(".", x, y)
+    } g.drawString("o", x, y)
 
 }
